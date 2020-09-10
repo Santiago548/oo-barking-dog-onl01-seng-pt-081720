e94254e7f2be5e9a82f94aeea063ad4215f6ab47
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-10 22:08:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,6 @@
 # Your code goes here!
 class Dog
+
   def name=(dogs_name)
     @this_dogs_name = dogs_name
   end
@@ -8,14 +9,8 @@ class Dog
     @this_dogs_name
   end
 
-  def bark
-    puts "woof!"
-  end
+    def bark
+      puts 'woof!'
+    end
+
 end
-
-fido = Dog.new
-fido.name =("Fido")
-
-fido.name
-
-fido.bark
